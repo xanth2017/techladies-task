531a4c3ff939d8525cf8281f10a959dfa802f68f
SW <xanth.t2017@gmail>
2019-09-24 22:24:37 +0800

    Add content and css to about page

--- whatsup/src/components/About.js
+++ whatsup/src/components/About.js
@@ -3,7 +3,8 @@ import s from "./About.module.scss";
 
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 function About() {
@@ -11,15 +12,29 @@ function About() {
         <div className={s.About}>
             <Jumbotron fluid className={s.customJumbo}>
                 <Container>
-                    {/* <Image src="/me.jpg" rounded  className={s.customMe}/>    */}
                     <h1 className={s.swivelChair} >Su Wei aka @Rebootlifer</h1>
                     <p>
-                    This is a modified jumbotron that occupies the entire horizontal space of
-                    its parent.
+                    “Resilience is accepting your new reality, even if it's less good than the one you had before. 
+                    You can fight it, you can do nothing but scream about what you've lost, or you can accept that and 
+                    try to put together something that's good.”
+                    ― Elizabeth Edwards
                     </p>
                  </Container>
-
             </Jumbotron>
+            <Container className={s.customIntro} >
+                <Row >
+                <Col md={{ span: 8, offset: 2 }} >
+                <h1>Introduction</h1>
+                <p> Why? @RebootLifer? RebootLifer was the spur of the moment brainchild of mine while i was trying 
+                    to improvise on how to deliver engaging course materials to my students. At the same time,
+                    having ample time to help them learn the materials.
+                </p>
+                <p>
+                    
+                </p>
+                </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
